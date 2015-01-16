f6e84f1a4546239c9deec664047d212b12dbe67d
Denis <denkuzenkov@gmail.com>
2015-01-16 20:23:12 +0000

    modified urls for create new contact

--- contacts/urls.py
+++ contacts/urls.py
@@ -3,5 +3,5 @@ from contacts.views import ListContactView, CreateContactView
 
 urlpatterns = patterns('',
     url(r'^$', ListContactView.as_view(), name='contacts-list',),
-    url(r'^new/', CreateContactView.as_view(), name='contacts-new',),
+    url(r'^new/$', CreateContactView.as_view(), name='contacts-new',),
 )
