405dc7c2442d188eb26ac49be5bdf5132945186c
Denis <denkuzenkov@gmail.com>
2015-01-16 20:01:03 +0000

    added exeption for py[code] files to .gitignore

--- contacts/views.py
+++ contacts/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from django.core.urlresolvers import reverse
+from django.views.generic import ListView, CreateView
+from contacts.models import Contact
 
-# Create your views here.
+
+class ListContactView(ListView):
+      model = Contact
+      template_name = 'contact_list.html'
+
+
+class CreateContactView(CreateView):
+    model = Contact
+    template_name = 'edit_contact.html'
+    fields = ['first_name', 'last_name', 'email']
+    
+    def get_success_url(self):
+        return reverse('contacts-list')
