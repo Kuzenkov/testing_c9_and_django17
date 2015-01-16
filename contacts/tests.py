b0e79c240f9f9879e06121f65eb0eb168f27714f
Denis <denkuzenkov@gmail.com>
2015-01-16 20:24:16 +0000

    Added test for testing ContactListView class

--- contacts/tests.py
+++ contacts/tests.py
@@ -1,6 +1,9 @@
 from django.test import TestCase
+from django.test.client import Client, RequestFactory
 
 from contacts.models import Contact
+from contacts.views import ListContactView
+
 
 class ContactTests(TestCase):
     """Contact model tests."""
@@ -8,3 +11,30 @@ class ContactTests(TestCase):
     def test_str(self):
         contact = Contact(first_name='John', last_name='Smith')
         self.assertEquals(str(contact), 'John Smith',)
+        
+
+class ContactListViewTests(TestCase):
+    """Contact list view tests."""
+    
+    def test_contacts_in_the_context(self):
+    
+        client = Client()
+        
+        response = client.get('/')
+        self.assertEquals(list(response.context['object_list']), [])
+        
+        Contact.objects.create(first_name='foo', last_name='bar')
+        response = client.get('/')
+        self.assertEquals(response.context['object_list'].count(), 1)
+        
+    def test_contacts_in_the_context_request_factory(self):
+    
+        factory = RequestFactory()
+        request = factory.get('/')
+        
+        response = ListContactView.as_view()(request)
+        self.assertEquals(list(response.context_data['object_list']), [])
+        
+        Contact.objects.create(first_name='foo', last_name='bar')
+        response = ListContactView.as_view()(request)
+        self.assertEquals(response.context_data['object_list'].count(), 1)
