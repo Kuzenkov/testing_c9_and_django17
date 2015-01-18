705143f920844f3c9c5510ddcda9baee3752df39
Denis <denkuzenkov@gmail.com>
2015-01-18 18:57:41 +0000

    Seting up urls for static files

--- test_app/urls.py
+++ test_app/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 import contacts
 
 urlpatterns = patterns('',
@@ -10,3 +11,5 @@ urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
     url(r'^', include('contacts.urls')),
 )
+
+urlpatterns += staticfiles_urlpatterns()
